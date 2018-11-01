e388d60b0f206259fe0a2bd3c22e410bee520cb2
Ammo Goettsch <817160+derammo@users.noreply.github.com>
2018-11-01 15:27:41 -0400

    disabled debug output

--- src/rewards/main.ts
+++ src/rewards/main.ts
@@ -1,6 +1,6 @@
 import { Configuration } from "./configuration";
 import { start } from "derlib/roll20/plugin";
 
-debug.log = console.log;
+// debug.log = console.log;
 
 start('rewards', Configuration);
