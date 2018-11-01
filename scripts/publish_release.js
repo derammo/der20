03fb9cc324ba08160221195b6a2cc0787c99f148
Ammo Goettsch <817160+derammo@users.noreply.github.com>
2018-11-01 15:27:11 -0400

    release notes sections now collapse if empty

--- scripts/publish_release.js
+++ scripts/publish_release.js
@@ -1,9 +1,12 @@
-const publishRelease = require('publish-release')
+const publishRelease = require('publish-release');
 const execSync = require('child_process').execSync;
 const fs = require('fs');
 
 function getHistory(paths) {
-    return execSync(`git log --date=short --format="##### %h by %an on %ad%n%w(0,4,4)%s%n%n%w(0,4,4)%N%b%n" $(git describe --tags --abbrev=0 HEAD~1)..HEAD -- ${paths}`, { encoding: 'utf-8' });    
+    return execSync(
+        `git log --date=short --format="##### %h by %an on %ad%n%w(0,4,4)%s%n%n%w(0,4,4)%N%b%n" $(git describe --tags --abbrev=0 HEAD~1)..HEAD -- ${paths}`,
+        { encoding: 'utf-8' }
+    );
 }
 
 const token = execSync('security find-internet-password -a derammo -s api.github.com -w', { encoding: 'utf-8' }).trim();
@@ -11,22 +14,30 @@ const version = process.argv[2];
 const common = process.argv[3];
 const groups = process.argv.slice(4);
 const tag = `v${version}`;
-const assets = fs.readdirSync(`releases/${version}`).map((name) => {
-    return `releases/${version}/${name}`
+const assets = fs.readdirSync(`releases/${version}`).map(name => {
+    return `releases/${version}/${name}`;
 });
 const notes = [];
 for (let group of groups) {
-    notes.push(`### Changes: ${group.replace(/^.*\//g, '')}`);
-    notes.push(getHistory(group));
+    let changes = getHistory(group);
+    if (changes.length > 0) {
+        notes.push(`### Changes: ${group.replace(/^.*\//g, '')}`);
+        notes.push();
+    }
 }
-notes.push('### Changes: Common')
-notes.push(getHistory(common));const releaseSpec = {
+let commonChanges = getHistory(common);
+if (commonChanges.length > 0) {
+    notes.push('### Changes: Common');
+    notes.push(commonChanges);
+}
+
+const releaseSpec = {
     token: token,
     owner: 'derammo',
     repo: 'der20',
     tag: tag,
     name: `der20 Roll20 API Scripts Release ${tag}`,
-    notes:  notes.join('\n'),
+    notes: notes.join('\n'),
     draft: true,
     prerelease: true,
     reuseRelease: true,
@@ -43,4 +54,4 @@ notes.push(getHistory(common));const releaseSpec = {
 
 publishRelease(releaseSpec, (err, release) => {
     console.log(release);
-})
+});
